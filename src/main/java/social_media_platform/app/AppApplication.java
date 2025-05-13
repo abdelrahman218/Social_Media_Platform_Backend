1c1ed49d0ed2dd5c0a52ed4b2bde91a1f514831b
Abdelrahman Alkot <abdelrahman.alkot2182004@gmail.com>
2025-05-13 10:21:34 +0300

    Project Intialization

---
+++ src/main/java/social_media_platform/app/AppApplication.java
@@ -0,0 +1,13 @@
+package social_media_platform.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/social_media_platform/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package social_media_platform.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
