f14e4647f61b5bdb1001c5c3c71257de90f5db92
YousefAli-1 <yousefalisaied@gmail.com>
2025-05-27 10:29:07 +0300

    bug fixes + logout + signup & login validation

--- src/main/java/social_media_platform/app/controllers/UserController.java
+++ src/main/java/social_media_platform/app/controllers/UserController.java
@@ -1,7 +1,5 @@
 package social_media_platform.app.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import social_media_platform.app.services.PostsService;
@@ -11,12 +9,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import social_media_platform.app.models.User;
+import social_media_platform.app.models.LoginRequest;
+import social_media_platform.app.repositories.UserRepository;
 
 
 @RestController
